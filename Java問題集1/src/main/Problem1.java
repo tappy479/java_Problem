3da77d40d9ed16b7ff2e7dd82a72d87cdcc6845a
tappy479 <tappi479@gmail.com>
2024-08-19 16:46:56 +0900

    問題集のフォルダと問題1を新規作成

---
+++ Java問題集1/src/main/Problem1.java
@@ -0,0 +1,11 @@
+package main;
+/*
+ * @author 小笠原健太
+ * Hello Worldと出力させてください
+ */
+public class Problem1 {
+		public static void main(String[] args) {
+			System.out.println("Hello World");
+			
+		}
+}
